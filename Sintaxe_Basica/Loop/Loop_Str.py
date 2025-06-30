a5dd3a790b374a70580aec72832cac99b6c72265
Ikaro <ikaro.j.s.m@gmail.com>
2025-06-30 16:44:08 -0300

    Exercício loop str

---
+++ Sintaxe_Basica/Loop/Loop_Str.py
@@ -0,0 +1,11 @@
+# Repetição de String 
+
+nome = 'Ikaro'
+
+for soletrar in nome:
+    print(f'{soletrar} está no nome {nome}')
+
+print('------------------------------------------------------')
+nome2 = 'Camila'
+for soletrar2 in nome2:
+    print(F'A letra {soletrar2} está no nome {nome2} ')
