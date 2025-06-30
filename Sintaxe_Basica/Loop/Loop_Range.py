d98dcb60981ce5899b5ca8c70786a69385c5c0f1
Ikaro <ikaro.j.s.m@gmail.com>
2025-06-30 16:29:18 -0300

    Exercícios de Loop

---
+++ Sintaxe_Basica/Loop/Loop_Range.py
@@ -0,0 +1,10 @@
+# Repetição de números
+
+''''for numero in range (5):
+    print(numero)'''
+
+'''for numero2 in range (0, 44, 4):
+    print(numero2)'''
+
+for numero3 in range (1, 16):
+    print(numero3)
